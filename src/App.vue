3df2c1d733cadae0b716d7130a1c377b2ef3a24f
dbapril <vladimir.vmax@gmail.com>
2020-04-28 07:09:54 +0300

    a landing page is completed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="content-wrapper">
+    <header class="header">
+      <h1 class="header__title">Информация о пациентах</h1>
+    </header>
+    <main class="main">
+      <img
+        class="illustration"
+        src="./components/illustration/illustration.svg"
+        alt="women with laptop"
+      />
+      <section class="profile-filling">
+        <h2 class="profile-filling__title">Заполните свой профиль и получите 100 баллов</h2>
+        <p class="profile-filling__info">
+          Заполните базовую информацию о себе, чтобы мы могли подбирать для вас
+          интересные акции и статьи. Информация о пациенте используется только
+          для{{"\u00A0"}}автозаполнения формы записи и{{"\u00A0"}}не{{"\u00A0"}}видна другим пользователям.
+        </p>
+        <div class="button-wrapper">
+          <button type="button" class="filling-button">
+            <span class="filling-button__text">Перейти к заполнению</span>
+            <span class="filling-button__delimiter"></span>
+            <img src="./components/filling-button/union-icon.png" alt class="filling-button__union" />
+            <span class="filling-button__text">+100</span>
+          </button>
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Form from "./components/Form.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Form
   }
-}
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "assets/scss/style.scss";
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="form">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Form'
+}
+</script>
+
+<style scoped lang="scss">
+@import 'src/assets/scss/form.scss';
+</style>
