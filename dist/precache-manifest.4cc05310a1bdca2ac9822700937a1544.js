bcc5100d916a76e0e9a886d0ab43d2fe5369e9fb
dbapril <vladimir.vmax@gmail.com>
2020-04-28 07:49:52 +0300

    git add

---
+++ dist/precache-manifest.4cc05310a1bdca2ac9822700937a1544.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a366bdb9a5b26b100059",
+    "url": "/css/app.9cd57fb0.css"
+  },
+  {
+    "revision": "a7e7458ca8354c3a9a23016784a4bc6c",
+    "url": "/fonts/Montserrat-Bold.a7e7458c.eot"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/fonts/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "b55e6e808057b12beea40fdced35277c",
+    "url": "/fonts/Montserrat-Bold.b55e6e80.woff"
+  },
+  {
+    "revision": "12a2903a11ed79dbf85a0610a2f7d94e",
+    "url": "/fonts/Montserrat-Medium.12a2903a.eot"
+  },
+  {
+    "revision": "9f2ae36a53edcac31a212dbd56eb747b",
+    "url": "/fonts/Montserrat-Medium.9f2ae36a.woff"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/fonts/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "98bf0fc7667a81a7054b483a98e83ee4",
+    "url": "/fonts/Montserrat-SemiBold.98bf0fc7.woff"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/fonts/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "e51f11d32a4e2d6bc3a7201e23a67c14",
+    "url": "/fonts/Montserrat-SemiBold.e51f11d3.eot"
+  },
+  {
+    "revision": "3e3d09a9a06bfa0163cd30f870e57aa0",
+    "url": "/img/illustration.3e3d09a9.svg"
+  },
+  {
+    "revision": "37f45816e4df4a51906eed64e624178d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a366bdb9a5b26b100059",
+    "url": "/js/app.18f9f0e2.js"
+  },
+  {
+    "revision": "bdb4fc9b0b24655dcea4",
+    "url": "/js/chunk-vendors.cf2b2226.js"
+  },
+  {
+    "revision": "8319ad437f51fb8b947a26971bb82994",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
